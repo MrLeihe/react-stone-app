38409dd669e437f57d016afb9a8bd93933ca3926
stone <1484542698@qq.com>
2019-07-03 11:04:51 +0800

    docs async await compare

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -34,6 +34,8 @@ class Home extends React.Component {
 
   gen = function* () {
     // generarot函数
+    // *相当于async， yeild 相当于 await
+    console.log('gen---->', this)
     const res1 = yield this.sendRequest()
     console.log(res1)
   }
